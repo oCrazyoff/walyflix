38989dc7970065ea96e97d8904f5e53b065053b8
oCrazyoff <walyssonribeiro3@gmail.com>
2025-09-02 20:28:32 -0300

    feat: tela de buscar pronta e dashboard iniciada

--- adm/dashboard.php
+++ adm/dashboard.php
@@ -1,13 +1,91 @@
 <?php
 $titulo = "Dashboard";
 include __DIR__ . "/../includes/inicio.php";
+
+// funções dos cards
+function usuariosTotais()
+{
+    global $conexao;
+
+    $sql = "SELECT COUNT(*) as total FROM usuarios";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    $total = $resultado->fetch_assoc();
+    $stmt->close();
+    return $total['total'];
+}
+
+function filmesTotais()
+{
+    global $conexao;
+
+    $sql = "SELECT COUNT(*) as total FROM filmes";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    $total = $resultado->fetch_assoc();
+    $stmt->close();
+    return $total['total'];
+}
+
+function filmeDestaque()
+{
+    global $conexao;
+
+    $sql = "SELECT titulo FROM filmes WHERE destaque = 1";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    $total = $resultado->fetch_assoc();
+    $stmt->close();
+    return $total['titulo'];
+}
+
+function filmeMaisSalvo()
+{
+    global $conexao;
+
+    $sql = "SELECT f.id, f.titulo, COUNT(*) AS total_salvos
+            FROM minha_lista ml
+            JOIN filmes f ON f.id = ml.filme_id
+            GROUP BY f.id, f.titulo
+            ORDER BY total_salvos DESC
+            LIMIT 1;
+            ";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    $total = $resultado->fetch_assoc();
+    $stmt->close();
+    return $total['titulo'];
+}
 ?>
 <main>
     <div class="interface">
         <div class="titulo">
             <div class="txt-titulo">
                 <h2><i class="bi bi-columns-gap"></i> Dashboard</h2>
-                <p>Gerencie as categorias de filmes do WalyFlix</p>
+                <p>Bem-vindo de volta, <?= htmlspecialchars($_SESSION['nome']) ?>. Aqui está um resumo da sua
+                    plataforma.</p>
+            </div>
+        </div>
+        <div class="container-cards">
+            <div class="card">
+                <h3>Usuários Totais <i class="bi bi-people"></i></h3>
+                <span><?= usuariosTotais() ?></span>
+            </div>
+            <div class="card">
+                <h3>Filmes Cadastrados <i class="bi bi-film"></i></h3>
+                <span><?= filmesTotais() ?></span>
+            </div>
+            <div class="card">
+                <h3>Filme em Destaque <i class="bi bi-star"></i></h3>
+                <span><?= filmeDestaque() ?></span>
+            </div>
+            <div class="card">
+                <h3>Filme Mais Salvo <i class="bi bi-bookmark"></i></h3>
+                <span><?= filmeMaisSalvo() ?></span>
             </div>
         </div>
     </div>
--- pages/buscar.php
+++ pages/buscar.php
@@ -8,12 +8,23 @@ include __DIR__ . "/../includes/inicio.php";
             <h2 class="text-4xl font-bold">Buscar Filmes e Séries</h2>
             <p class="text-branco-texto-opaco text-xl">Encontre seus conteúdos favoritos</p>
         </div>
-        <div class="flex items-center justify-center my-3 w-full">
-            <input class="w-1/2 border border-borda h-full p-2 text-xl rounded-lg" type="search" name="buscar"
+        <div class="flex items-center justify-center my-5 w-full">
+            <input class="w-2/3 border border-borda h-full p-3 text-xl rounded-lg ring-2 ring-azul" type="search"
+                   name="buscar"
                    id="buscar"
                    placeholder="Buscar...">
         </div>
-        <h3 id="txt-resultados"></h3>
+        <div id="placeholder-buscar" class="flex flex-col items-center jutifiy-center w-full">
+            <i class="bi bi-search text-7xl text-branco-texto-opaco mt-10"></i>
+            <h3 class="text-3xl my-5">Comece a pesquisar</h3>
+            <p class="text-branco-texto-opaco text-xl text-center max-w-1/2">
+                Digite o nome do filme, série ou categoria que você está procurando na barra de busca acima.
+            </p>
+        </div>
+        <div class="flex items-center justify-between w-full text-xl">
+            <h3 id="txt-resultados"></h3>
+            <p id="qtd-resultados" class="text-branco-texto-opaco"></p>
+        </div>
         <div id="resultados" class="flex flex-wrap gap-3 mt-5"></div>
     </div>
 </main>
@@ -26,11 +37,19 @@ include __DIR__ . "/../includes/inicio.php";
                 .then(res => res.json())
                 .then(data => {
                     let container = document.getElementById("resultados");
+                    let txt_resultado = document.getElementById("txt-resultados");
+                    let qtd_resultado = document.getElementById("qtd-resultados");
+                    let placeholder = document.getElementById("placeholder-buscar");
+                    let resultados = 0;
                     container.innerHTML = "";
 
 
                     if (data.length > 0) {
+                        placeholder.style.display = "none";
+                        txt_resultado.innerHTML = `Resultados para "${query}"`;
                         data.forEach(filme => {
+                            resultados++;
+                            qtd_resultado.innerHTML = `${resultados} resultados`
                             let imagem = filme.imagem_url ? filme.imagem_url
                                 : "https://www.protrusmoto.com/wp-content/uploads/revslider/home5/placeholder-1200x500.png";
 
